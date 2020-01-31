c59f1ad3d9bf84843ad200e838dbfcfea23281dd
Brittany Houtz <brittany.houtz@gmail.com>
2020-01-30 16:01:10 -0800

    set up resty file shell

---
+++ src/components/Resty.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Form from './Form.js';
+
+export default class Madlib extends Component{
+  state = {
+
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+  }
+
+  render() {
+    const {  } = this.state;
+    return (
+      <>
+
+      </>
+    );
+  }
+}
