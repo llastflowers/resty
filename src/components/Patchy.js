04b2c29821c19459dc3c8a5df76576c61ab1c6f5
Brittany Houtz <brittany.houtz@gmail.com>
2020-01-30 17:56:14 -0800

    start handleSubmit

--- src/components/Patchy.js
+++ src/components/Patchy.js
@@ -4,16 +4,19 @@ import Form from './Form.js';
 export default class Patchy extends Component{
   state = {
     URL: '',
+    method: '',
     rawJSON: '',
   }
 
   handleChange = ({ target }) => {
     this.setState({ [target.name]: target.value });
   }
-  // handleSubmit = event => {
-  //   event.preventDefault();
-
-  // }
+  handleSubmit = event => {
+    event.preventDefault();
+    fetch(this.state.URL);
+    //get response and do something with it
+    //console log response
+  }
 
   render() {
     return (
